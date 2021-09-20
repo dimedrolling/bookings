3a5e769c7e1c08ecdf578ab10bef1a557e01f0a8
dima <dimavg2001@gmail.com>
2021-09-20 23:22:53 +0300

    Playing with csrf tokens and post methods

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/dimedrolling/bookings/pkg/config"
 	"github.com/dimedrolling/bookings/pkg/models"
 	"github.com/dimedrolling/bookings/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -33,7 +36,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	stringMap["host"] = host
-	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
 }
@@ -48,7 +51,61 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 	host := r.Host
 	m.App.Session.Put(r.Context(), "host", host)
-	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+
+// Reservation renders the make a reservation page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Attic renders the make a room page
+func (m *Repository) Attic(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "attic.page.tmpl", &models.TemplateData{})
+}
+
+// Magic renders the make a room page
+func (m *Repository) Magic(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "magic.page.tmpl", &models.TemplateData{})
+}
+
+// Availability renders the make a check-availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// PostAvailability post the form from  the make a check-availability page
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handle request for availability from each room page
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+// Contact renders the make a check-availability page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
